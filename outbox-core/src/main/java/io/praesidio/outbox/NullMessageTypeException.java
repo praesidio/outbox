68742599aff57e26a6c649bdc8e1cb26ab47a529
Michał Cybulski <michal@cybulski.me>
2020-10-21 14:08:57 +0200

    Add missing messages to exceptions

--- outbox-core/src/main/java/io/praesidio/outbox/NullMessageTypeException.java
+++ outbox-core/src/main/java/io/praesidio/outbox/NullMessageTypeException.java
@@ -2,5 +2,6 @@ package io.praesidio.outbox;
 
 class NullMessageTypeException extends RuntimeException {
     NullMessageTypeException(Class<? extends MessageSerializer> aClass) {
+        super("Class does not provide a valid message type:" + aClass.getName());
     }
 }
--- outbox-core/src/main/java/io/praesidio/outbox/SerializerDuplicationException.java
+++ outbox-core/src/main/java/io/praesidio/outbox/SerializerDuplicationException.java
@@ -4,5 +4,6 @@ import io.praesidio.outbox.values.MessageType;
 
 class SerializerDuplicationException extends RuntimeException {
     SerializerDuplicationException(MessageType messageType) {
+        super("Duplicate serialziers for message type: " + messageType.getValue());
     }
 }
